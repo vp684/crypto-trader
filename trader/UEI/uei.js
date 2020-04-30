cf4edaaab6bf94fc3b0a6e6674798d17b344a482
Viraj Patel <viraj84@gmail.com>
2020-04-30 12:26:21 -0400

    UEI

--- trader/UEI/uei.js
+++ trader/UEI/uei.js
@@ -1,3 +1,25 @@
+"use strict";
 
 
+class UEI {
+    
 
+    /**
+     * 
+     * @param {String} exchange string for exchange to send to must be coinbase, gemini, binance
+     * @param {market: String, qty:Number, price: Number, type: String} order object with order details type must be limit or market
+     */    
+    buy(exchange, order){
+        
+
+    }
+
+    
+    sell(exchange, order){
+
+    }
+
+    connectWS(){
+        
+    }
+}
