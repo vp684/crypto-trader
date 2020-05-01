5759f03bc25977d92f1f37648f528186e592e1e8
Viraj Patel <viraj84@gmail.com>
2020-05-01 13:06:44 -0400

    fix limiters

--- trader/exchanges/gemini/gemini_rest.js
+++ trader/exchanges/gemini/gemini_rest.js
@@ -1,8 +1,9 @@
-const GeminiAPI = require('gemini-api').default
+"use strict";
 require('dotenv').config()
+const GeminiAPI = require('gemini-api').default
 const sleep = require('../../../helper/sleep').sleep
 const logger = require('../../../helper/logger')
-const api = new GeminiAPI({key: process.env.GEMINI_KEY, secret:  process.env.GEMINI_SECRET})
+
 
  
 
@@ -12,12 +13,13 @@ class Gemini_REST{
         this.last_public_call = 0
         this.privateQue= []
         this.last_private_call = 0
-
+        this.api = new GeminiAPI({key: process.env.GEMINI_KEY, secret:  process.env.GEMINI_SECRET})
         //time in ms between calls
         this.public_limit = 1000 // 120 per min no more than 1 per sec
         this.private_limit = 200 // 600 per min no moe than 5 per sec.
 
         this.publicLimiter()
+        this.privateLimiter()
     }
 
     async publicLimiter(){
@@ -26,7 +28,7 @@ class Gemini_REST{
             if(this.publicQue.length > 0){
                 let curtime = new Date().getTime()
                 for(let i = 0; i < this.publicQue.length; i++){                
-                    let newpubtime = this.last_public_call + this.public_delay                    
+                    let newpubtime = this.last_public_call + this.public_limit                    
                     if(curtime > newpubtime){
                         this.last_public_call = curtime
                         this.publicQue[i].func()
@@ -51,9 +53,9 @@ class Gemini_REST{
             if(this.privateQue.length > 0){
                 let curtime = new Date().getTime()
                 for(let i = 0; i < this.privateQue.length; i++){                
-                    let newpubtime = this.last_public_call + this.public_delay                    
-                    if(curtime > newpubtime){
-                        this.last_public_call = curtime
+                    let newprivtime = this.last_private_call + this.private_limit                    
+                    if(curtime > newprivtime){
+                        this.last_private_call = curtime
                         this.privateQue[i].func()
                         this.privateQue.splice(i, 1)
                     }
@@ -61,30 +63,28 @@ class Gemini_REST{
                 }
 
             }
-            await sleep(10) //  time to check between calls that fail to meet min delay  
-            this.publicLimiter()       
+            await sleep(20) //  time to check between calls that fail to meet min delay  
+            this.privateLimiter()       
         }
         catch(e){
            logger.error('Gemini REST public limiter error', e)
-            await sleep(10)
-            this.publicLimiter() 
+            await sleep(20)
+            this.privateLimiter() 
         }
 
 
     }
+
+    sendOrder(market, qty, price, side){
+
+        api.newOrder({
+            "symbol": "btcusd", 
+            "amount": "0.005", 
+            "price": "6915.00",
+            "side": "buy"
+        })
+
+    }
 }
-
-setTimeout(()=>{
-    api.newOrder({
-        "symbol": "btcusd", 
-        "amount": "0.005", 
-        "price": "6915.00",
-        "side": "buy"
-    })
-
-   // api.getNotionalVolume()
-
-}, 5000)
-
 
 module.exports = Gemini_REST
